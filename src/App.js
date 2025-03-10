458eff072fcd6ec80110caf90421e87bfb2b7d61
Kyac99 <116833043+Kyac99@users.noreply.github.com>
2025-03-10 21:46:18 +0100

    Add src directory with core application files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Dashboard from './pages/Dashboard';
+import Screener from './pages/Screener';
+import StockDetail from './pages/StockDetail';
+import { IndexProvider } from './contexts/IndexContext';
+
+function App() {
+  return (
+    <Router>
+      <IndexProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/screener" element={<Screener />} />
+            <Route path="/stock/:symbol" element={<StockDetail />} />
+          </Routes>
+        </Layout>
+      </IndexProvider>
+    </Router>
+  );
+}
+
+export default App;
